09bb1076b5006556c4ed4c643f9616cf32173339
Cyril <achat567@yahoo.fr>
2022-01-18 13:19:02 +0100

    récupération des données de yahoo finance avec pandas

---
+++ recuperation_donnees.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# sp_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+# sp = pd.read_html(sp_url, header=0)[0]  # returns a list for each table
+# sp.info()
+
+
+
