6be318be5dbd74c87b31286675e5b5f58a894cd0
Alexis <95538222+JayroZeppeli@users.noreply.github.com>
2026-06-01 12:00:18 +0200

    Delete recuperation_donnees.py

--- recuperation_donnees.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# sp_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-# sp = pd.read_html(sp_url, header=0)[0]  # returns a list for each table
-# sp.info()
-
-
-
